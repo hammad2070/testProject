41a802b8c5ec6211758fa23c84e583f2a3c53d97
check <check@localhost>
2022-11-11 16:11:31 -0500

    Print 1 to 10

--- Test/src/Practicee.java
+++ Test/src/Practicee.java
@@ -2,6 +2,11 @@ public class Practicee {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		int i;
+		for(i=1; i<=10; i++) {
+			System.out.println(i);	
+		}
+		System.out.println("");
 
 	}
 
